238cfefbf974f0832b201556d972abeb259e99ab
aruna seun <arunaseun@yahoo.com>
2018-10-27 15:34:06 +0100

    Freelancer project with angular6

---
+++ src/homepage/homepage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+ @Component({
+ moduleId: 'module.id',
+	selector: 'app',
+	template: '<h1>Freelance Bootcamp Dashboard</h1>'
+})
+export class HomepageComponent {}
